4eeb64a47279b18b6f14ced10397e78a6c7f4ca5
maoziyue <>
2017-10-23 21:27:20 +0800

    config ok

---
+++ gitignore/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gitignore
+//
+//  Created by maoziyue on 2017/10/23.
+//  Copyright © 2017年 maoziyue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ gitignore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitignore
+//
+//  Created by maoziyue on 2017/10/23.
+//  Copyright © 2017年 maoziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
